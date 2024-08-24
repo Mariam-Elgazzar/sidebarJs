f88dba6939b6c2a6b783670c24feeaa7cd7234dc
Ahmed Muhammed <AhmedMuhammed0031@gmail.com>
2024-08-24 16:31:25 +0300

    Add Cards With style

---
+++ script.js
@@ -0,0 +1,11 @@
+// const word = document.querySelector('.word')
+// const overlayDiv = document.querySelector('.overlay')
+// const svgIcon = document.querySelector('.svg-icon')
+// console.log(word)
+// console.log(overlayDiv)
+// const show = function (e) {
+//     overlayDiv.style.backgroundColor = '#33333396';
+//     svgIcon.style.opacity = 0;
+// };
+// word.addEventListener('mouseover', show.bind(this));
+// // word.removeEventListener('mouseover', show());
