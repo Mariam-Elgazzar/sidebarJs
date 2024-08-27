758e25046e838d5f8dadcfa0d819db4809555b6f
Ahmed Muhammed <AhmedMuhammed0031@gmail.com>
2024-08-27 18:16:20 +0300

    Add SideBar

--- script.js
+++ script.js
@@ -9,3 +9,23 @@
 // };
 // word.addEventListener('mouseover', show.bind(this));
 // // word.removeEventListener('mouseover', show());
+// const sidebar = document.querySelector('.sidebar')
+// document.querySelector('.try').addEventListener('click', function () {
+//     sidebar.classList.remove('sidebar')
+//     sidebar.classList.add('sidebarv')
+// })
+
+document.querySelector('.try').addEventListener('click', function () {
+    document.getElementById("sidenav").style.width = "100%";
+    document.querySelector('.black-menu').style.width = "25%"
+    document.querySelector('.black-menu').style.opacity = "1"
+
+
+});
+
+document.querySelector('.closebtn').addEventListener('click', function () {
+    document.getElementById("sidenav").style.width = "0";
+    document.querySelector('.black-menu').style.opacity = "0"
+    document.querySelector('.black-menu').style.width = "0"
+
+});
